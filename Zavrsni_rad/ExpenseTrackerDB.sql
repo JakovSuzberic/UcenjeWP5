7a8819a4c25151c292f05361dec97ab08bdac486
Jakov Šužberić <jakov.suzberic@gmail.com>
2024-06-02 15:57:10 +0200

    Updated syntax error
    Updated User to "User" because User is reserved keyword

--- Zavrsni_rad/ExpenseTrackerDB.sql
+++ Zavrsni_rad/ExpenseTrackerDB.sql
@@ -6,7 +6,7 @@ create database ExpenseTrackerDB;
 go
 use ExpenseTrackerDB;
 
-CREATE TABLE User (
+CREATE TABLE "User" (
 
   user_id int not null primary key identity(1,1),
   name varchar(50) not null,
@@ -34,13 +34,13 @@ CREATE TABLE Payment_method (
 CREATE TABLE Budget (
 
   budget_id int not null primary key identity(1,1),
-  user_id int,
-  category_id int,
+  user_id int not null,
+  category_id int not null,
   amount float,
   start_date datetime,
   end_date datetime,
 
-  FOREIGN KEY (user_id) REFERENCES User(user_id),
+  FOREIGN KEY (user_id) REFERENCES "User"(user_id),
   FOREIGN KEY (category_id) REFERENCES Category(category_id)
 
 );
@@ -51,11 +51,11 @@ CREATE TABLE Expense (
   user_id int,
   category_id int,
   payment_method_id int,
-  ammount float,
+  amount float,
   date datetime,
   description varchar(255),
 
-  FOREIGN KEY (user_id) REFERENCES User(user_id),
+  FOREIGN KEY (user_id) REFERENCES "User"(user_id),
   FOREIGN KEY (category_id) REFERENCES Category(category_id),
   FOREIGN KEY (payment_method_id) REFERENCES Payment_method(payment_method_id)
 
