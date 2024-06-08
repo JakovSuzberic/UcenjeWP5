0d27bffef5345851f253cd1c00e0a4284652bd7f
Jakov Šužberić <jakov.suzberic@gmail.com>
2024-06-08 23:17:01 +0200

    Update ExpenseTrackerDB_update_delete

--- Zavrsni_rad/ExpenseTrackerDB_update_delete.sql
+++ Zavrsni_rad/ExpenseTrackerDB_update_delete.sql
@@ -1,3 +1,19 @@
-UPDATE Budgets SET start_date = '2024-06-05 00:00' WHERE budget_id = 1;
+﻿
 
-SELECT * FROM Budgets;
+UPDATE Budgets SET start_date = '2024-06-05 00:00' WHERE budget_id = 1;
+UPDATE Users SET name = 'Mijo Mijić' WHERE user_id = 3;
+UPDATE Users SET email = 'mijomijic@gmail.com' WHERE user_id = 3;
+
+DELETE FROM Expenses WHERE expense_id = 3;
+--DELETE FROM Payment_methods WHERE name = 'Cash';
+
+
+--Pitati kako ovo zaobici?
+
+
+
+INSERT INTO Expenses(amount, date, description,user_id,category_id,payment_method_id) VALUES
+(59.99,'2024-06-08 23:00','Groceries',3,3,3);
+
+SELECT * FROM Expenses;
+SELECT * FROM Payment_methods;
