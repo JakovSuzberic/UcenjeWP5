12b83b1a5d1c6097616bd7c629fb75cf0b3f92ad
Jakov Šužberić <jakov.suzberic@gmail.com>
2024-06-14 13:59:01 +0200

    Surfanje

--- Ucenje/svastara.sql
+++ Ucenje/svastara.sql
@@ -1,5 +1,30 @@
-
+﻿
 
 select * from artikli;
+select * from ArtikliNaPrimci;
+select * from Dobavljaci;
+select * from Mjesta;
+select * from Proizvodi;
+select * from Usluge;
+
+
+-- idete u svatove i  zelite kupiti poklon u vrijednostiod 100 do 150 eura koliko artikala je na izboru?
+
+select * from Artikli where cijena >= 100 and cijena <= 150;
+
+-- taj artikl mora bitikućanski aparat ?
+
+select * from Artikli where cijena >= 100 and cijena <= 150; 
+
+
+select Artikli.dugiNaziv  as Naziv, Artikli.barkod as BARKOD from Artikli; 
+
+
+select * from Artikli where dugiNaziv like '%rukavica%';
+
+select * from Artikli where dugiNaziv like '%auto%';
+
+select * from Artikli where dugiNaziv like 'auto%';
+
 
 
