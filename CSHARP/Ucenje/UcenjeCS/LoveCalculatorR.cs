5651181fe7a02a050edd846a3ffbd7b23a0d0f93
Jakov Šužberić <jakov.suzberic@gmail.com>
2024-08-06 22:50:10 +0200

    Create LoveCalculatorR

---
+++ CSHARP/Ucenje/UcenjeCS/LoveCalculatorR.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class LoveCalculatorR
+    {
+
+        public static void izvedi()
+        {
+
+            //01.GET USER INPUT FOR NAMES
+            Console.WriteLine("Enter the name for first person: ");
+
+            string name1 = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Enter the name for the second person: ");
+
+            string name2 = Console.ReadLine().ToUpper();
+
+             
+
+
+
+
+        }
+
+
+    }
+}
